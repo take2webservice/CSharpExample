79d6e21f8fcaaeb61081d0b309ceea63e5ec3e35
Taketo Iseki <take2webservice@gmail.com>
2023-07-04 19:27:51 +0900

    Hello World

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
